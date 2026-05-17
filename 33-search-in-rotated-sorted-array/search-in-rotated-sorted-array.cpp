a7fc159a2ba21b58abc2cdc615bd5d03620661e0
Shubham Kumar <shubham210804@gmail.com>
2026-05-17 13:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.37%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+// int getPivot(vector<int>& nums){
+//     int n = nums.size();
+//     int s = 0;
+//     int e = n-1;
+//     int mid = s + (e-s)/2;
+    
+//     while(s<e){
+//         if(nums[mid]>=nums[0]){
+//             s=mid+1;
+//         }
+//         else{
+//             e=mid;
+//         }
+//         mid = s + (e-s)/2;
+//     }
+//     return s;
+// }
+
+// int binSearch(vector<int>& nums,int s, int e, int target){
+//     int start = s;
+//     int end = e;
+//     int mid = start + (end-start)/2;
+
+//     while(start<=end){
+//         if(nums[mid]==target){
+//             return mid;
+//         }
+//         if(target>nums[mid]){
+//             start = mid+1;
+//         }
+//         else{
+//             end = mid-1;
+//         }
+//         mid = start + (end-start)/2;
+//     }
+//     return -1;
+// }
+    int search(vector<int>& nums, int target) {
+    //     int pivot = getPivot(nums);
+    //     if(target>=nums[pivot] && target<=nums[nums.size()-1]){
+    //         return binSearch(nums,pivot,nums.size()-1,target);
+    //     }
+    //     else{
+    //         return binSearch(nums,0,pivot-1,target);           
+    //     }
+    
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        if(nums[i]==target){
+            return i;
+        }
+    }
+    return -1;
+    }
+};
